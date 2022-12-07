4426f032a81ba9b6a67b4d14eaa567d7e712ed0d
minaalbert33 <89715882+minaalbert33@users.noreply.github.com>
2022-12-07 21:20:32 +0200

    adding some functions and solving template issue

--- mmVector.cpp
+++ mmVector.cpp
@@ -1,8 +1,10 @@
 #include "mmVector.h"
+
 template<typename object>
 mmVector<object>::mmVector(int n)
         :itsSize(n), itsCapacity(n), ptr(new object[n])
 {}
+
 template<typename object>
 mmVector<object>::mmVector(object* arr, int n):itsSize(n), itsCapacity(n), ptr(new object[n]){
     for(int i = 0; i < itsSize; i++){
@@ -10,11 +12,95 @@ mmVector<object>::mmVector(object* arr, int n):itsSize(n), itsCapacity(n), ptr(n
     }
 
 }
+template<typename object>
+mmVector<object>::mmVector(const mmVector& temp){
+    itsSize = temp.itsSize;
+    itsCapacity = temp.itsCapacity;
+    ptr = new object[itsSize];
+
+    for(int i = 0; i < itsSize; i++){
+        ptr[i] = temp.ptr[i];
+    }
+}
+
+
+template<typename object>
+void mmVector<object>::push_back(object temp){
+    if(itsCapacity > itsSize) ptr[itsSize - 1] = temp;
+    else{
+        increaseCapacity(2 * itsSize);
+        ptr[itsSize] = temp;
+        itsSize++;
+    }
+}
+
+template<typename object>
+object& mmVector<object>::operator[](int index){
+    return ptr[index];
+}
+
+template<typename object>
+const object& mmVector<object>::operator[](int index) const{
+    return ptr[index];
+}
+
+template<typename object>
+object mmVector<object>::pop_back(){
+    object temp = ptr[itsSize - 1];
+    ptr[itsSize - 1] = object();
+    itsSize--;
+    return temp;
+}
+//template<typename object>
+//iterator mmVector<object>::begin(){
+//    return
+//}
 
 //template<typename object>
-//void mmVector<object>::print() const {
-//    for(int i = 0; i < 5; i++){
-//        cout << ptr[i] << ' ';
-//    }
+//void mmVector<object>::erase(iterator){
+//
 //}
+
+// 1 2 3 5
+// 1 2 5
+
+template<typename object>
+void mmVector<object>::increaseCapacity(int n){
+    if(n <= itsCapacity) return;
+
+    itsCapacity = n;
+    object* newVector = new object[itsCapacity];
+    for(int i = 0; i < itsSize; i++){
+        newVector[i] = ptr[i];
+    }
+
+    delete[] ptr;
+    ptr = newVector;
+
+}
+
+template<typename object>
+mmVector<object>::~mmVector(){
+    delete[] ptr;
+
+}
+
+template<typename object>
+int mmVector<object>::size()const{
+    return itsSize;
+}
+
+template<class object>
+int mmVector<object>::capacity() const {
+    return itsCapacity;
+}
+
+
+template<typename object>
+void mmVector<object>::print() const {
+    for(int i = 0; i < itsSize; i++){
+        cout << ptr[i] << ' ';
+    }
+}
+
 
--- mmVector.h
+++ mmVector.h
@@ -5,43 +5,46 @@
 #include<iostream>
 using namespace std;
 template<class object>
+
 class mmVector {
     typedef object* iterator;
 private:
     object* ptr;
     int itsSize;
     int itsCapacity;
+    void increaseCapacity(int n);
 public:
 
     mmVector (int); // Initialize by specific capacity
     mmVector (object*, int ); // Initialize by n items from array
-//    mmVector (const mmVector&); // Initialize with a copy
+    mmVector (const mmVector&); // Initialize with a copy
     ~mmVector();
 //    mmVector &operator=(const mmVector&); // Copy assignment
 //    mmVector &operator=(const mmVector&&); // Move assignment
 //
 //    // Modifying operations
-//    object& operator[](int); // Access item by reference
-//    object operator[](int) const; // For const objects
-//    int push_back(object); // Add item to end of vec & return # of items
-//    object pop_back(); // Remove and return last element in vec
-//    void erase(iterator); // Remove item at iterator
+    object& operator[](int); // Access item by reference
+    const object& operator[](int) const; // For const objects
+    void push_back(object); // Add item to end of vec & return # of items
+    object pop_back(); // Remove and return last element in vec
+    void erase(iterator); // Remove item at iterator
+    iterator begin() ;// Return an iterator (T*)
+    iterator end(); // Return an iterator (T*)
 //    void erase(iterator, iterator);// Remove items between
 //    void clear(); // Delete all vector content
 //    void insert(iterator, object); // Insert item at iterator
-//    iterator begin() ;// Return an iterator (T*)
-//    iterator end(); // Return an iterator (T*)
+
 //
 //    // Comparison operations
 //    bool operator==(const mmVector<object>&) ;// Return true if ==
 //    bool operator< (const mmVector<object>&); // Compares item by item
 //
 //    // Capacity operations
-//    int size() const ;  // Return current size of vec
-//    int capacity() const; // Return size of current allocated array
+    int size() const ;  // Return current size of vec
+    int capacity() const; // Return size of current allocated array
 //    int resize() ; // Relocate to bigger space
 //    bool empty() ; // Return true if size is 0
-//    void print() const;
+    void print() const;
 //    // Friends
 //    friend ostream& operator << (ostream& out, mmVector<object>);
 };
