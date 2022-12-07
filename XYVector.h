3f203640e5b8c30e8428521764b2bd5c5d4bb481
minaalbert33 <89715882+minaalbert33@users.noreply.github.com>
2022-12-07 03:33:17 +0200

    edititng member variables

--- XYVector.h
+++ XYVector.h
@@ -1,35 +1,36 @@
 #include<iostream>
 using namespace std;
-template<typename T>
+template<typename object>
 
 class XYVector {
-    typedef T* iterator;
+    typedef object* iterator;
 private:
-    T* ptr;
-    int sze;
+    object* ptr;
+    int itsSize;
+    int itsCapacity;
 public:
 
     XYVector (int); // Initialize by specific capacity
-    XYVector (T*, int n ); // Initialize by n items from array
+    XYVector (object*, int n ); // Initialize by n items from array
     XYVector (const XYVector&); // Initialize with a copy
     ~XYVector();
     XYVector &operator=(const XYVector&); // Copy assignment
     XYVector &operator=(const XYVector&&); // Move assignment
 
     // Modifying operations
-    T& operator[](int); // Access item by reference
-    int push_back(T); // Add item to end of vec & return # of items
-    T pop_back(); // Remove and return last element in vec
+    object& operator[](int); // Access item by reference
+    int push_back(object); // Add item to end of vec & return # of items
+    object pop_back(); // Remove and return last element in vec
     void erase(iterator); // Remove item at iterator
     void erase(iterator, iterator);// Remove items between
     void clear(); // Delete all vector content
-    void insert(iterator, T); // Insert item at iterator
+    void insert(iterator, object); // Insert item at iterator
     iterator begin() ;// Return an iterator (T*)
     iterator end(); // Return an iterator (T*)
 
     // Comparison operations
-    bool operator==(const XYVector<T>&) ;// Return true if ==
-    bool operator< (const XYVector<T>&); // Compares item by item
+    bool operator==(const XYVector<object>&) ;// Return true if ==
+    bool operator< (const XYVector<object>&); // Compares item by item
 
     // Capacity operations
     int size() const ;  // Return current size of vec
@@ -38,5 +39,5 @@ public:
     bool empty() ; // Return true if size is 0
 
     // Friends
-    friend ostream& operator << (ostream& out, XYVector<T>);
+    friend ostream& operator << (ostream& out, XYVector<object>);
 };
